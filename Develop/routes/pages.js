5436e51fbe1df85245768ad510430cdbb3ce0bc0
ricanlonghorn23 <luis.ra.aldaz@gmail.com>
2024-05-06 22:47:51 -0500

    Edit for render

--- Develop/routes/pages.js
+++ Develop/routes/pages.js
@@ -2,7 +2,7 @@ const pageRouter = require('express').Router();
 const path = require('path');
 
 pageRouter.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 pageRouter.get('/notes', (req, res) =>{
